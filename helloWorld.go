59d9da4ce1646428970b768dc4936f59d6c77d82
Ruhika <65492460+Ruhika1417@users.noreply.github.com>
2022-01-17 21:37:28 +0530

    hello world in go

---
+++ helloWorld.go
@@ -0,0 +1,12 @@
+// every program is part of a package
+package main
+
+/*import ("fmt") lets us import
+files included in the fmt package*/
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
